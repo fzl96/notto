dfc7783b92af218bc42927e5b363a73c97cef10a
seika333 <fpsdragon333@gmail.com>
2022-07-21 03:13:38 +0700

    add layout animation

--- src/components/notes/Card.tsx
+++ src/components/notes/Card.tsx
@@ -2,7 +2,7 @@ import type { NoteType } from "../../utils/types";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { format } from "date-fns";
 import { useMantineColorScheme } from "@mantine/core";
-
+import { AnimatePresence, motion } from "framer-motion";
 interface Props {
   note: NoteType;
 }
@@ -19,18 +19,29 @@ const Card: React.FC<Props> = ({ note }) => {
   }
 
   return (
-    <div className={`${dark? "bg-grayishDark" : "bg-white"} rounded-2xl p-5 shadow-sm min-h-[10rem]`}>
-      <div className="flex flex-col justify-between h-full">
-        <div>
-          <h3 className="text-lg font-semibold">{note.title}</h3>
-          <p className="text-md">{note.body}</p>
+    // <AnimatePresence>
+      <motion.div
+        layout
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        // transition={{ duration: 1 }}
+        className={`${
+          dark ? "bg-grayishDark" : "bg-white"
+        } rounded-2xl p-5 shadow-sm min-h-[10rem]`}
+      >
+        <div className="flex flex-col justify-between h-full">
+          <div>
+            <h3 className="text-lg font-semibold">{note.title}</h3>
+            <p className="text-md">{note.body}</p>
+          </div>
+          <div className="flex items-center gap-3">
+            <AiOutlineCalendar />
+            <p className="uppercase text-sm font-semibold">{createdAt}</p>
+          </div>
         </div>
-        <div className="flex items-center gap-3">
-          <AiOutlineCalendar />
-          <p className="uppercase text-sm font-semibold">{createdAt}</p>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+    // </AnimatePresence>
   );
 };
 export default Card;
--- src/pages/app/notes/Notes.tsx
+++ src/pages/app/notes/Notes.tsx
@@ -6,6 +6,7 @@ import NewNoteButton from "../../../components/notes/NewNoteButton";
 import NewNoteModal from "../../../components/notes/NewNoteModal";
 import { useNotesContext } from "../../../hooks/UseNotesContext";
 import { NoteType } from "../../../utils/types";
+import { motion } from 'framer-motion';
 
 const Notes = () => {
   const [open, setOpen] = useState(false);
@@ -16,12 +17,12 @@ const Notes = () => {
       <div className="flex flex-col gap-5 lg:px-10 h-full">
         <div className="flex flex-col gap-5">
           <h1 className="text-2xl  font-semibold ml-1">Pinned</h1>
-          <div className="grid lg:grid-cols-3 grid-cols-2 gap-4">
+          <motion.div className="grid lg:grid-cols-3 grid-cols-2 gap-4">
             {notes.map((note: NoteType, index: number) => {
-              if (index > 2) return null;
+              // if (index > 2) return null;
               return <Card key={note.id} note={note} />;
             })}
-          </div>
+          </motion.div>
         </div>
         <div className="flex flex-col gap-5">
           <h1 className="text-2xl font-semibold ml-1">Recent notes</h1>

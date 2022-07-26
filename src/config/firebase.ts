cae9724181fe4d7b154a5e71549ef310e60ad3f2
fzl96 <fpsdragon333@gmail.com>
2022-07-26 15:38:08 +0700

    enable persistence firestore

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -14,4 +14,18 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
+
 export { db }; 
